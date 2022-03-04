072360f51b2fcee78bfc8884ff781ecfd96d6031
Your Name <you@example.com>
2022-03-04 13:30:33 +0000

    added 4 compass directions

--- game.py
+++ game.py
@@ -4,8 +4,18 @@ def main():
     print(" hi " + player_name)
     life_counter = 3
     print(f" you have   {life_counter}   remaining ")
-
-
+    print("you can do in 1 of 4 directions, North, East, South, West")
+    player_Direction = input("which direction would you like to go in?")
+    if player_Direction == "North":
+        print("you went North")
+    elif player_Direction == "East":
+        print("you went East")
+    elif player_Direction == "South":
+        print("you went South")
+    elif player_Direction == "West":
+        print("you went West")
+    else:
+        print("sorry not recognised")
 
 
 main()
