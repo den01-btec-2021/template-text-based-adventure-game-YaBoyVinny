16cf212a29c1b9d020c311a86b7528af0780417a
Your Name <you@example.com>
2022-03-04 13:13:48 +0000

    added life counter

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 def main():
     print("welcome to my game!")
-    player_name = input("what is your name")
-    print("hi" + player_name)
+    player_name = input(" what is your name ")
+    print(" hi " + player_name)
+    life_counter = 3
+    print(f" you have   {life_counter}   remaining ")
+
+
+
+
 main()
