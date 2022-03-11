8d7367678f703e0c60e92bc3c14f9b14f68dfbf3
Your Name <you@example.com>
2022-03-11 14:42:21 +0000

    basically finished

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ def main():
 
             if player_Direction == "North":
                 print("you went North")
-                # i am trying to gail in puzzle, lose life
+                # i am trying to fail in puzzle, lose life
                 PuzzleGuessNorth = input("what is 10+10")
                 if PuzzleGuessNorth == "20":
                     print("correct")
@@ -33,10 +33,42 @@ def main():
 
             elif player_Direction == "East":
                 print("you went East")
+                # repeat of the code above
+                PuzzleGuessEast = input("what was the name of the man that made the tesla coil ")
+                if PuzzleGuessEast == "Nikola tesla":
+                    print("correct")
+                    Backpack.append("key 2")
+                
+                else:
+                    print("incorrect")
+                    life_counter -= 1
+                    print(f"you have lost a life, you now have {life_counter} remianing")
+
+
             elif player_Direction == "South":
                 print("you went South")
+                # repeat of North
+                print("A: going to the pub, B: getting food, C: getting girls or D: all of the above")
+                PuzzleGuessSouth = input("what is Kian Smith known for?")
+                if PuzzleGuessSouth == "all of the above":
+                    print("correct")
+                    Backpack.append("key 3")
+
+                else:
+                    print("incorrect")
+                    life_counter -= 1
+                    print("you have lost a life, you now have {life_counter} remaining") 
+            
             elif player_Direction == "West":
                 print("you went West")
+                #
+                PuzzlegameWest = input("was this a good game")
+                if PuzzlegameWest == "No":
+                    print("thank you for your honesty")
+                    Backpack.append("key 4")
+                else:
+                    print("your lying")
+            
             else:
                 print("sorry not recognised")
 
